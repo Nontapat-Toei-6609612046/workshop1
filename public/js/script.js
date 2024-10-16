0604ea0a7ce3e3e2a4bccb59025a6c3bacefe7ec
Nontapat Toei - 6609612046 <144689358+Nontapat-Toei-6609612046@users.noreply.github.com>
2024-10-16 21:10:05 +0700

    Code Updated

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,52 @@
-document.getElementById("helloBtn").addEventListener("click", function() {
-    alert("Hello!");
-});
-
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+function submitLogin(event) {
     event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    
+   
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const message = document.getElementById('message');
+    
+    message.innerText = '';
 
-    if (username && password) {
-        alert("Logged in successfully!");
-    } else {
-        alert("Please fill in both fields.");
+   
+    if (!username) {
+        message.innerText = 'Please enter your username';
+        return;
     }
-});
+    
+    if (!password) {
+        message.innerText = 'Please enter your password';
+        return;
+    }
+    
+   
+    if (password.length < 13) {
+        message.innerText = 'Password must be 13 characters long';
+        return;
+    }
+
+    // หาก input ถูกต้อง ทำการส่งข้อมูลไปยัง API
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Application-Key': 'TU55aa8b138006376c4dcb1ef750c1bdf761918dfb31f9fea51449474b7f1301294072158d1fa7d2a3f287fce0291651f2'
+        },
+        body: JSON.stringify({
+            UserName: username, 
+            PassWord: password
+        })
+    })
+    .then(response => response.json())
+    .then(data => {  
+        if (data.displayname_th) {
+            message.innerText = data.displayname_th;
+        } else {
+            message.innerText = 'Login failed. Please try again';
+        }
+    })
+    .catch(error => {
+        message.innerText = 'Error: ' + error.message;
+        console.error('Error:', error);
+    });
+}
